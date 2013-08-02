503dd2dd2740947163036fd7fc76b329d8cd4e9f
Charles Perkins <chuck@rputbl.com>
2013-08-02 11:41:52 -0700

    factor hashbase code to library

--- rputn.go
+++ rputn.go
@@ -26,23 +26,16 @@
 package main
 
 import (
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"net/url"
 	"flag"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
-	"crypto"
-	"crypto/rand"
-	"crypto/rsa"
-	_ "crypto/sha1"
-	"crypto/sha256"
-	"crypto/x509"
-	"encoding/pem"
+
+        "rputbl.com/hashbase"
 )
 
 var hashrepo = flag.String("h", "rputbl.com", "The hash repository address")
@@ -52,49 +45,6 @@ var verbose = flag.Bool("v", false, "Verbose logging")
 var query = flag.Bool("q", false, "Query hash of file")
 var introduce = flag.Bool("i", false, "Introduce myself to the hash repository")
 var asrtstr = flag.String("a", "none", "Comma separated ssertions about the file")
-
-// sha224base64 performs a sha224 hash on a byte array and then perfroms a base64 encoding on the result.
-func sha224base64(item []byte) (string, []byte) {
-
-	phash := sha256.New224()
-	io.WriteString(phash, string(item))
-	phashbytes := phash.Sum(nil)
-	return base64.StdEncoding.EncodeToString(phashbytes), phashbytes
-}
-
-// sign64 signs a byte array with a private key.
-func sign64(rsakey *rsa.PrivateKey, item []byte) (string, []byte) {
-
-	hashFunc := crypto.SHA1
-	h := hashFunc.New()
-	h.Write(item)
-	digest := h.Sum(nil)
-	signresult, _ := rsa.SignPKCS1v15(rand.Reader, rsakey, hashFunc, digest)
-	return base64.StdEncoding.EncodeToString(signresult), signresult
-}
-
-// getPKI retrieves 'rputn' RSA public and private key values from the users ~/.ssh diretory or
-// instructs the user to generate rputn RSA public and private key files.
-func getPKI() (*rsa.PrivateKey, []byte, error) {
-
-	rsa_file := fmt.Sprintf("%s/.ssh/rputn_rsa", os.Getenv("HOME"))
-	rsapub_file := fmt.Sprintf("%s/.ssh/rputn_rsa.pub", os.Getenv("HOME"))
-
-	_, err := os.Stat(rsa_file)
-	if err == nil {
-		_, err = os.Stat(rsapub_file)
-	}
-	if err != nil {
-		return nil, nil, errors.New("Please generate a reputation public/private key pair, e.g.:\n#ssh-keygen -t rsa -C \"<username>@<hostname>\" -f ~/.ssh/rputn_dsa\n")
-	}
-
-	rputn_rsa, _ := ioutil.ReadFile(rsa_file)
-	rputn_rsa_pub, _ := ioutil.ReadFile(rsapub_file)
-	block, _ := pem.Decode(rputn_rsa)
-	rsakey, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
-
-	return rsakey, rputn_rsa_pub, nil
-}
 
 // process will introduce the user (based on an RSA public key) or
 // generate hashes of local files and query a server about those hashes or
@@ -111,12 +61,12 @@ func process() error {
 		return errors.New("command must only be a query (-q) or an assertion (-a) or an introduction (-i)")
 	}
 
-	rsakey, rputn_rsa_pub, err := getPKI()
+	rsakey, rputn_rsa_pub, err := hashbase.GetPKI()
 	if err != nil {
 		return err
 	}
 
-	phstr, _ := sha224base64(rputn_rsa_pub)
+	phstr, _ := hashbase.Sha224base64(rputn_rsa_pub)
 
 	hloc := ""
 
@@ -131,7 +81,7 @@ func process() error {
 				fmt.Printf("Hashing: %s : ", f)
 			}
 
-			fhstr, fhbytes := sha224base64([]byte(f))
+			fhstr, fhbytes := hashbase.Sha224base64([]byte(f))
 
 			if *query {
 				hloc = fmt.Sprintf("http://%s:%d/q?%s", *hashrepo, *hashport, fhstr)
@@ -143,7 +93,7 @@ func process() error {
 
 			if *asrtstr != "none" {
 				combinedBytes := append(fhbytes, *asrtstr...)
-				signresultstr, _ := sign64(rsakey, combinedBytes)
+				signresultstr, _ := hashbase.Sign64(rsakey, combinedBytes)
 
 				if 1 == 2 {
 					hloc = fmt.Sprintf("http://%s:%d/a?%s&%s&%s&%s", *hashrepo, *hashport, fhstr, phstr, *asrtstr, signresultstr)
